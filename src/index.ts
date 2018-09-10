59a234272f7eed5841c61614118d0e39e10def30
Joel Lonbeck <joel@neutrinographics.com>
2018-09-10 15:32:00 -0700

    exposed some more classes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 export {default} from "./WordMap";
+export {default as Parser} from "./Parser";
+export {default as Engine} from "./Engine";
+export {default as Algorithm} from "./Algorithm";
+// structures
 export {default as Alignment} from "./structures/Alignment";
 export {default as Ngram} from "./structures/Ngram";
 export {default as Prediction} from "./structures/Prediction";
